ceede241bd1d32d07afa19d28596492f6b313580
djeang <djeangdev@yahoo.fr>
2020-03-05 00:52:18 +0100

    clean multi-techno

--- html5-multi-techno/springbootapp/src/main/java/springboot/Application.java
+++ html5-multi-techno/springbootapp/src/main/java/springboot/Application.java
@@ -4,17 +4,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Application implements WebMvcConfigurer {
 
     private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
+    private final Environment env;
+
+    Application(Environment environment) {
+        this.env = environment;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -23,12 +32,14 @@ public class Application implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         Path webDist = Paths.get("../web/dist");
-        if (!Files.exists(webDist)) {
-            webDist = Paths.get("webDist");
-        }
-        if (Files.exists(webDist)) {
-            //LOG.info("Adding {} to static resources", webDist);
-            //registry.addResourceHandler("/**").addResourceLocations("file:" + webDist);
+        if (Arrays.asList(env.getActiveProfiles()).contains("dev")) {
+            if (!Files.exists(webDist)) {
+                webDist = Paths.get("webDist");
+            }
+            if (Files.exists(webDist) && Arrays.asList(env.getActiveProfiles()).contains("dev")) {
+                LOG.info("Adding {} to static resources", webDist);
+                registry.addResourceHandler("/**").addResourceLocations("file:" + webDist);
+            }
         }
     }
 

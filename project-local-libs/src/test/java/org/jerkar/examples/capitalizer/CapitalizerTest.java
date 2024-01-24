78dd3449c445c3dfb23527ea5fc2a02a7f412dae
djeang <djeangdev@yahoo.fr>
2024-01-25 00:30:13 +0100

    Adapt

---
+++ project-local-libs/src/test/java/org/jerkar/examples/capitalizer/CapitalizerTest.java
@@ -0,0 +1,14 @@
+package org.jerkar.examples.capitalizer;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CapitalizerTest {
+
+	@Test
+	public void test() {
+		String sample = "hello world";
+		Assertions.assertEquals("Hello World", Capitalizer.capitalize(sample));
+	}
+
+}
